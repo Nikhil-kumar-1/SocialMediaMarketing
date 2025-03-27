1489b37d5dc7ed0bd5d8390aecff4d1fffd95157
ankuraaryan5 <ankuraaryan5@gmail.com>
2025-03-27 13:08:43 +0530

    changes in app.jsx

--- digital-marketing/src/App.jsx
+++ digital-marketing/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
