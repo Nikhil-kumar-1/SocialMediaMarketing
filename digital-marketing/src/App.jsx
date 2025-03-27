e986f399062ce76ec988f1bbdd6ccdbff4893b6e
ankuraaryan5 <ankuraaryan5@gmail.com>
2025-03-27 13:14:35 +0530

    add Navbar component to App.jsx

--- digital-marketing/src/App.jsx
+++ digital-marketing/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 import Home from './components/Home'
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={
           <>
           <Header />
+          <Navbar />
           <Home/>
           </>
         } />
