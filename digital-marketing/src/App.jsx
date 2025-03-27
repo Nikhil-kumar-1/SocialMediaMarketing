6ef67dc240ee7146ce4ad887bc4db68652a4f955
Nikhil-kumar-1 <kumarnikhil787031@gmail.com>
2025-03-27 13:06:44 +0530

    second commit

--- digital-marketing/src/App.jsx
+++ digital-marketing/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Header from './components/Header'
 
+import Header from './components/Header'
+import Home from './components/Home'
 function App() {
   return (
     <Router>
@@ -10,7 +10,7 @@ function App() {
         <Route path="/" element={
           <>
           <Header />
-          <Navbar />
+          <Home/>
           </>
         } />
       </Routes>
